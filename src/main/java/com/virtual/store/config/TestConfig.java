7f34ab94ee394c69a41f134d3f79cdfae8271474
CarloSilva96 <carlosilva.dev@gmail.com>
2021-12-05 10:31:35 -0300

    Criado profile de desenvolvimento e adicionado estrategia para criacao de dados no bd

--- src/main/java/com/virtual/store/config/TestConfig.java
+++ src/main/java/com/virtual/store/config/TestConfig.java
@@ -1,16 +1,32 @@
 package com.virtual.store.config;
 
+import com.virtual.store.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.text.ParseException;
+
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class TestConfig {
+    @Autowired
+    private DBService dbService;
+
+    /** CRIANDO ATRIBUTO PARA VALIDAR A CRIACAO DO BANCO SOMENTE QUANDO O DDL FOR CREATE **/
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddlGeracaoBanco;
 
     @Bean
-    public boolean instanciandoBandoDeDados(){
+    public boolean instanciandoBandoDeDados() throws ParseException {
+        /** SE FOR DIFERENTE DE CREATE ELE ENTRA E NAO RECRIA O BANCO **/
+        if (!"create".equals(ddlGeracaoBanco)){
+            return false;
+        }
 
+        dbService.instanciandoTesteBancoDeDados();
         return true;
     }
 
